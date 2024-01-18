9bdd81aa67a50efbfbe896f75f5bd994f97d344d
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-01-18 11:27:27 +0530

    Update 4rotimg.py

--- MultidimensionalArray/4rotimg.py
+++ MultidimensionalArray/4rotimg.py
@@ -23,10 +23,3 @@ def rotate(matrix: list[list[int]]) -> None:
         else:
             j+=1
             k-=1
-
-
-matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
-rotate(matrix)
-print(matrix)
-
-#[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
