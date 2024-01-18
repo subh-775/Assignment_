82e8247abdd0ad071a92c2076aaba5dd6d6ed5c7
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-01-18 11:23:13 +0530

    Update 9mergesrt.py

--- Array/9mergesrt.py
+++ Array/9mergesrt.py
@@ -22,11 +22,3 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         nums1[i] = nums2[b]
         b -= 1
         i -= 1
-
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
-
-merge(nums1, m, nums2, n)
-print(nums1)
