5209defed3f43218680cddb2bb2b27a03d457182
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-01-18 10:59:11 +0530

    Add files via upload

---
+++ 2subset.py
@@ -0,0 +1,17 @@
+# ALL SUBSETS
+
+def subsets(nums: list[int]) -> list[list[int]]:
+    def backtrack(start, path):
+
+        result.append(path[:])
+
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+
+    result = []
+    backtrack(0, [])
+    return result
+nums = [1, 2, 3]
+print(subsets(nums))
