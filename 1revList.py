06b9e55de718b22330ab978e88713c1d42ce568f
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-01-18 11:13:01 +0530

    Add files via upload

---
+++ 1revList.py
@@ -0,0 +1,18 @@
+# REVERSE GIVEN LINKED LIST
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def reverseList(head: ListNode) -> ListNode:
+        prev = None
+        curr = head
+        while curr is not None:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
