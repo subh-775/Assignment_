0557acc9b10c96fbfec0add6df75d37c87c9d8f4
Ashirwad - San <ashirwadrai5879@outlook.com>
2024-01-18 11:29:03 +0530

    Update 2kthsmallest.py

--- SearchingAndSorting/2kthsmallest.py
+++ SearchingAndSorting/2kthsmallest.py
@@ -13,5 +13,3 @@ def kthSmallest(matrix: list[list[int]], k: int) -> int:
             else:
                 column += 1
         return sorted(lst)[k-1]
-
-print(kthSmallest([[1,5,9],[10,11,13],[12,13,15]], k = 8))
